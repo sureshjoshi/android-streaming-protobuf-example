b5db25f259ac24f0530d43dae12928746184f7fb
SJ <suresh@vicarasolutions.com>
2016-03-28 17:20:27 +0800

    Created an example app to perform streaming protobuffer operations.

---
+++ app/src/main/java/com/sureshjoshi/android/streamingprotobufexample/utils/WireUtils.java
@@ -0,0 +1,42 @@
+package com.sureshjoshi.android.streamingprotobufexample.utils;
+
+import com.squareup.wire.Message;
+import com.squareup.wire.ProtoAdapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import okio.BufferedSink;
+import okio.BufferedSource;
+import okio.Okio;
+
+public class WireUtils {
+
+    public static void encodeWithLengthPrefix(OutputStream outputStream, List<Message> messages) throws IOException {
+        for (Message message : messages) {
+            encodeWithLengthPrefix(outputStream, message);
+        }
+    }
+
+    public static void encodeWithLengthPrefix(OutputStream outputStream, Message message) throws IOException {
+        int size = message.adapter().encodedSize(message);
+        BufferedSink sink = Okio.buffer(Okio.sink(outputStream));
+        sink.writeIntLe(size);
+        message.encode(sink);
+        sink.emit();
+    }
+
+    public static <M extends Message> List<M> decodeWithLengthPrefix(InputStream inputStream, ProtoAdapter<M> adapter) throws IOException {
+        List<M> messages = new ArrayList<>();
+        BufferedSource source = Okio.buffer(Okio.source(inputStream));
+        while (!source.exhausted()) {
+            int size = source.readIntLe();
+            byte[] bytes = source.readByteArray(size);
+            messages.add(adapter.decode(bytes)); // TODO: Reflect this or something
+        }
+        return messages;
+    }
+}
